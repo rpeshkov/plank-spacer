833d9af271d68c467815b2aa78fd5f1fd010d7f8
Denys Dovhan <email@denysdovhan.com>
2015-03-07 23:00:04 +0200

    Create new spacer.desktop for every space Case statement has been removed.

--- plank-spacer.sh
+++ plank-spacer.sh
@@ -1,39 +1,30 @@
 #!/bin/bash
 
-spacerapp="$HOME/.local/share/applications/spacer.desktop"
+UUID=$(cat /proc/sys/kernel/random/uuid)
+spacerapp="$HOME/.local/share/applications/spacer-$UUID.desktop"
+spacerDockItem="$HOME/.config/plank/dock1/launchers/spacer-$UUID.dockitem"
 
-case "$1" in
-    add)
-        if [ ! -f $spacerapp ]
-        then
-            echo "Spacer app not found."
-            echo "Copying blank.png to $HOME/.icons/"
+echo "Make new spacer app..."
+if [ ! -f "$HOME/.icons/blank.png"]; then
+    echo "Copying blank.png to $HOME/.icons/"
+    mkdir -p "$HOME/.icons/"
+    cp "./blank.png" "$HOME/.icons/"
+fi
 
-            mkdir -p "$HOME/.icons/"
-            cp "./blank.png" "$HOME/.icons/"
-
-
-            echo "Creating spacer.desktop file"
-            cat <<EOF > $spacerapp
+echo "Creating new spacer.desktop file"
+cat <<EOF > $spacerapp
 [Desktop Entry]
 Version=1.0
 Type=Application
-Name=Spacer
+Name=Space
 Exec=nothing
 Icon=$HOME/.icons/blank.png
 NoDisplay=true
 EOF
-        fi
 
-        UUID=$(cat /proc/sys/kernel/random/uuid)
-        spacerDockItem="$HOME/.config/plank/dock1/launchers/spacer-$UUID.dockitem"
-
-        cat <<EOF > $spacerDockItem
+cat <<EOF > $spacerDockItem
 [PlankItemsDockItemPreferences]
-Launcher=file://$HOME/.local/share/applications/spacer.desktop
+Launcher=file://$HOME/.local/share/applications/spacer-$UUID.desktop
 EOF
 
-        echo "New spacer dock item created with name \"$spacerDockItem\""
-        ;;
-esac
-
+echo "New spacer dock item created with name \"spacer-$UUID\""

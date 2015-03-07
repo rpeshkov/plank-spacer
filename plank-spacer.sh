37907afb09a9d893b0fe5da6f24c686e0435937e
Denys Dovhan <email@denysdovhan.com>
2015-03-07 23:11:46 +0200

    Fix missing ']'

--- plank-spacer.sh
+++ plank-spacer.sh
@@ -5,7 +5,7 @@ spacerapp="$HOME/.local/share/applications/spacer-$UUID.desktop"
 spacerDockItem="$HOME/.config/plank/dock1/launchers/spacer-$UUID.dockitem"
 
 echo "Make new spacer app..."
-if [ ! -f "$HOME/.icons/blank.png"]; then
+if [ ! -f "$HOME/.icons/blank.png" ]; then
     echo "Copying blank.png to $HOME/.icons/"
     mkdir -p "$HOME/.icons/"
     cp "./blank.png" "$HOME/.icons/"
